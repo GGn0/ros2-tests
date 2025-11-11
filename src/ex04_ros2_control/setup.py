0ff4d6b5ce1570b5a0034abf7689f93a65503267
ggn0 <luigi.s1994@gmail.com>
2025-11-11 18:41:06 +0100

    Add exercise 4 package

---
+++ src/ex04_ros2_control/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'ex04_ros2_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ggn0',
+    maintainer_email='luigi.s1994@gmail.com',
+    description='Example using ROS2 control framework',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
